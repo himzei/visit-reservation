3eadb4f40c0ab09b2c3aea3bfb4e498f61444031
himzei <himzei@gamil.com>
2023-08-31 17:47:58 +0900

    teacher page

--- src/app/teacher/TeacherHistory.jsx
+++ src/app/teacher/TeacherHistory.jsx
@@ -1,3 +1,4 @@
+import "./TeacherHistory.css";
 import React from "react";
 import Layout from "../../components/Layout";
 import { TEACHER_LIST } from "../../lib/menuList";
@@ -5,7 +6,52 @@ import { TEACHER_LIST } from "../../lib/menuList";
 export default function TeacherHistory() {
   return (
     <Layout menu={TEACHER_LIST}>
-      <div></div>
+      <div className="teacher-history">
+        {/* 검색 */}
+        <div className="teacher-history__search">
+          <span>기간</span>
+          <input type="text" />
+          ~
+          <input type="text" />
+          <select>
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <option key={i} value={i}>
+                  선택옵션 {i}
+                </option>
+              ))}
+          </select>
+        </div>
+
+        {/* 테이블 */}
+        <table>
+          <thead>
+            <tr>
+              <td>방문객명</td>
+              <td>차량번호</td>
+              <td>방문예정일시</td>
+              <td>목적</td>
+              <td>예약일시</td>
+              <td>상태</td>
+            </tr>
+          </thead>
+          <tbody>
+            {Array(10)
+              .fill("")
+              .map((_, i) => (
+                <tr key={i}>
+                  <td>홍길동</td>
+                  <td>42누 1234</td>
+                  <td>2023-10-16 16:30</td>
+                  <td>진학상담</td>
+                  <td>2023-10-14 16:00</td>
+                  <td>방문 2023-10-14 16:00</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
 }
--- src/app/teacher/TeacherRegister.jsx
+++ src/app/teacher/TeacherRegister.jsx
@@ -1,11 +1,76 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import RegIcon2 from "../../assets/svg/security-register2.svg";
+import RegIcon3 from "../../assets/svg/security-register3.svg";
 import { TEACHER_LIST } from "../../lib/menuList";
+import { Button } from "@chakra-ui/react";
 
 export default function TeacherRegister() {
   return (
     <Layout menu={TEACHER_LIST}>
-      <div></div>
+      <div className="security-register">
+        <section>
+          <div className="reg-title">
+            <img src={RegIcon2} alt="icon2" />
+            <h2>방문객 정보</h2>
+          </div>
+          <div className="input-group">
+            <div>방문객명</div>
+            <input type="text" placeholder="성함을 입력해 주세요." />
+          </div>
+          <div className="input-group">
+            <div>휴대전화번호</div>
+            <input type="text" placeholder="휴대전화번호를 입력해 주세요." />
+          </div>
+          <div className="input-group">
+            <div>차량번호</div>
+            <input type="text" placeholder="차량번호를 입력해 주세요." />
+          </div>
+        </section>
+        <section>
+          <div className="reg-title">
+            <img src={RegIcon3} alt="icon3" />
+            <h2>방문장소 정보</h2>
+          </div>
+          <div className="input-group">
+            <div>방문자</div>
+            <select>
+              <option className="select-default" value="">
+                방문자를 선택해 주세요.
+              </option>
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <option key={i} value={i}>
+                    선택옵션 {i}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="input-group">
+            <div>방문목적</div>
+            <select placeholder="방문목적" className="select">
+              <option className="select-default" value="">
+                방문목적을 선택해 주세요.
+              </option>
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <option key={i} value={i}>
+                    선택옵션 {i}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="input-group">
+            <div>방문일시</div>
+            <input type="text" placeholder="차량번호를 입력해 주세요." />
+          </div>
+        </section>
+        <div className="btn-container">
+          <Button colorScheme="blue">등록하기</Button>
+        </div>
+      </div>
     </Layout>
   );
 }
